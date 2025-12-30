f43a41d3222e49ec68c3105e2aa7f0b2bd5ae793
Aswin Suresh Kumar <asure12@illinois.edu>
2025-12-29 23:00:09 -0600

    First commit

---
+++ src/main/java/com/y_aswin/ChiTownStudentApartments/ChiTownStudentApartmentsApplication.java
@@ -0,0 +1,13 @@
+package com.y_aswin.ChiTownStudentApartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiTownStudentApartmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiTownStudentApartmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/y_aswin/ChiTownStudentApartments/ChiTownStudentApartmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y_aswin.ChiTownStudentApartments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiTownStudentApartmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
