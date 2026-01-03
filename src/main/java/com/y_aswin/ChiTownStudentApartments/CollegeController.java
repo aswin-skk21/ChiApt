44c023183dfbcb81dd554fb3ca7127f5738ded61
Aswin Suresh Kumar <asure12@illinois.edu>
2026-01-03 16:09:59 -0600

    reset all branches

--- src/main/java/com/y_aswin/ChiTownStudentApartments/CollegeController.java
+++ src/main/java/com/y_aswin/ChiTownStudentApartments/CollegeController.java
@@ -1,4 +1,9 @@
 package com.y_aswin.ChiTownStudentApartments;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/college")
 public class CollegeController {
 }
