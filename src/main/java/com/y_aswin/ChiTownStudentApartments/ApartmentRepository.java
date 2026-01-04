c6ca78f30ddd1886820037704a8a713cbc5a1e04
Aswin Suresh Kumar <asure12@illinois.edu>
2026-01-04 17:41:55 -0600

    Created apartment service class

---
+++ src/main/java/com/y_aswin/ChiTownStudentApartments/ApartmentRepository.java
@@ -0,0 +1,6 @@
+package com.y_aswin.ChiTownStudentApartments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApartmentRepository extends JpaRepository<Apartment, Integer> {
+}
