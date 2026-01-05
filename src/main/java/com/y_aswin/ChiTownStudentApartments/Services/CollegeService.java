9611e519746128df925f89126e4b80e43910044a
Aswin Suresh Kumar <asure12@illinois.edu>
2026-01-04 18:49:46 -0600

    created college service class

---
+++ src/main/java/com/y_aswin/ChiTownStudentApartments/Services/CollegeService.java
@@ -0,0 +1,24 @@
+package com.y_aswin.ChiTownStudentApartments.Services;
+
+import com.y_aswin.ChiTownStudentApartments.Entities.Apartment;
+import com.y_aswin.ChiTownStudentApartments.Entities.College;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CollegeService {
+
+    private final CollegeService collegeService;
+
+
+    public CollegeService(CollegeService collegeService) {
+        this.collegeService = collegeService;
+    }
+
+    public void getAllColleges() {
+    }
+
+    public void insertApartment(Apartment apt) {
+    }
+}
