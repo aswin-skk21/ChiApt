6efa04856d1f3cec4d481e870de822cd3d90b963
Aswin Suresh Kumar <asure12@illinois.edu>
2026-01-06 19:29:35 -0600

    created get apartment by college method

--- src/main/java/com/y_aswin/ChiTownStudentApartments/Repositories/ApartmentRepository.java
+++ src/main/java/com/y_aswin/ChiTownStudentApartments/Repositories/ApartmentRepository.java
@@ -1,7 +1,11 @@
 package com.y_aswin.ChiTownStudentApartments.Repositories;
 
 import com.y_aswin.ChiTownStudentApartments.Entities.Apartment;
+import com.y_aswin.ChiTownStudentApartments.Entities.College;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApartmentRepository extends JpaRepository<Apartment, Integer> {
+    List<College> getApartmentsById(int id);
 }
